98ec43fd9365f49afefebb647c6ec9470e8f3a9b
Ahmed Elbanawi <elbanawiahmed@gmail.com>
2021-07-07 11:46:42 +0200

    Add files via upload

--- Main/Student.c
+++ Main/Student.c
@@ -6,6 +6,7 @@
 /*****************************************************/
 STUDENT_t Student[STUDENT_MAX_NUMBER];
 int STUDENT_Max_Number = STUDENT_MAX_NUMBER;
+static char Flag = 1;
 static int Is_Char(const int SYMBOLE);
 /*****************************************************/
 void STUDENT_Init(void){
@@ -20,7 +21,12 @@ void STUDENT_Delete(void){
     unsigned int Id;
     printf("Press Enter To Delete Student Or Backspace To Return...\n >> ");
     printf(" Enter Student ID: ");
-    scanf("%*c%u", &Id);
+    if(1 == Flag){
+       scanf("%u", &Id);
+       Flag = 0;
+    }else{
+        scanf("%*c%u", &Id);
+    }
     if(Id < STUDENT_MAX_NUMBER && !Is_Char(Id)){
         printf(" Deleting Student .... Done\n");
         Student[Id].Id = -1;
@@ -34,7 +40,12 @@ void STUDENT_Add(void){
     unsigned int Id;
     printf("Press Enter To Add Student Or Backspace To Return...\n >> ");
     printf(" Enter Student ID: ");
-    scanf("%*c%u", &Id);
+    if(1 == Flag){
+       scanf("%u", &Id);
+       Flag = 0;
+    }else{
+        scanf("%*c%u", &Id);
+    }
     if(Id < STUDENT_MAX_NUMBER && -1 == Student[Id].Id && !Is_Char(Id)){
         Student[Id].Id = Id;
         printf(" >>  Enter Student Name: ");
@@ -49,7 +60,12 @@ void STUDENT_Find(void){
     unsigned int Id;
     printf("Press Enter To Find Student Or Backspace To Return...\n >> ");
     printf(" Enter Student ID: ");
-    scanf("%*c%u", &Id);
+    if(1 == Flag){
+       scanf("%u", &Id);
+       Flag = 0;
+    }else{
+        scanf("%*c%u", &Id);
+    }
     if(Id < STUDENT_MAX_NUMBER && -1 != Student[Id].Id && !Is_Char(Id)){
         printf(" >>  %s\n", Student[Id].Name);
     }else{
